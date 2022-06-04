3242801bda6bf49ee9f478c2ad005b271ad7d2e8
vansh1504.be21@chitkara.edu.in <vansh1504.be21@chitkara.edu.in>
2022-06-04 14:26:40 +0530

    Intitalize the keyboard file

---
+++ keyboard.js
@@ -0,0 +1,6 @@
+var button = document.getElementsByClassName("button"); 
+var display = document.getElementById("dis");
+var operand1 = null;
+var operand2 = null;
+var operator = null;
+var result = null;
